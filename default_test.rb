248020d94eb38415510d2c6ecbc1f6c050f70fc9
Larry Eichenbaum <larryebaum@getchef.com>
2019-05-06 19:01:38 -0400

    update
    update

--- default_test.rb
+++ default_test.rb
@@ -19,6 +19,6 @@ end
 
 describe http(‘http://localhost’, enable_remote_worker: true) do 
   its(‘status’)  { should cmp 200 }  
-  its(’body’)  {should cmp ‘Inspec Jumpstart’ }
+  its(’body’)  { should cmp ‘Inspec Jumpstart’ }
   its(‘headers-Content-type’)  { should cmp ‘text/html’ }
 end
