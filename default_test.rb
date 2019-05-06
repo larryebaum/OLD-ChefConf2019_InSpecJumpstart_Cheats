950544aa949b470441044124acab85da2d66dd0a
Larry Eichenbaum <larryebaum@getchef.com>
2019-05-06 19:24:08 -0400

    fix curly quotes

--- default_test.rb
+++ default_test.rb
@@ -17,8 +17,8 @@ describe port(80), :skip do
   it { should_not be_listening }
 end
 
-describe http(‘http://localhost’, enable_remote_worker: true) do 
-  its(‘status’)  { should cmp 200 }  
-  its(’body’)  { should cmp ‘Inspec Jumpstart’ }
-  its(‘headers-Content-type’)  { should cmp ‘text/html’ }
+describe http('http://localhost', enable_remote_worker: true) do 
+  its('status')  { should cmp 200 }  
+  its('body')  { should cmp 'Inspec Jumpstart' }
+  its('headers-Content-type')  { should cmp 'text/html' }
 end
