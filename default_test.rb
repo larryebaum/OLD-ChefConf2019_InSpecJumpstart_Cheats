a2a3b2dc94cfb9a2a7bb5ce0474a4a0511f2ba5a
Larry Eichenbaum <larryebaum@getchef.com>
2019-05-06 20:21:46 -0400

    fix method

--- default_test.rb
+++ default_test.rb
@@ -20,5 +20,5 @@ end
 describe http('http://localhost', enable_remote_worker: true) do 
   its('status')  { should cmp 200 }  
   its('body')  { should cmp 'Inspec Jumpstart' }
-  its('headers-Content-type')  { should cmp 'text/html' }
+  its('headers.Content-Type')  { should cmp 'text/html' }
 end
