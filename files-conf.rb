1231af4f410f7f79a731c0ea25f7220905ee489f
Larry Eichenbaum <larryebaum@getchef.com>
2019-05-07 20:26:03 -0400

    create

---
+++ files-conf.rb
@@ -0,0 +1,11 @@
+nginx_version: 1.10.3
+Mandatory_modules:
+  - http_ssl
+  - stream_ssl
+  - mail_ssl
+Auth_modules:
+  - http_auth_request
+  - nginx-auth-ldap
+Debian_modules:
+  - nginx-http-sysguard
+
