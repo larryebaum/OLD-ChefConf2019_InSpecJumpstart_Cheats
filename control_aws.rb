f6d8d18071b093f955af2bb4ec30ec33ce96a163
Larry Eichenbaum <larryebaum@getchef.com>
2019-05-09 09:08:59 -0400

    tabs

--- control_aws.rb
+++ control_aws.rb
@@ -1,13 +1,13 @@
 control 'myaws-01' do 
- title 'AWS Verification'
- desc 'Testing AWS'
- describe aws_ec2_instance('<YOUR INSTANCE ID>') do
-	it { should exist }
-	it { should be_running}
- end
+  title 'AWS Verification'
+  desc 'Testing AWS'
+  describe aws_ec2_instance('<YOUR INSTANCE ID>') do
+    it { should exist }
+    it { should be_running}
+  end
 
- describe aws_s3_bucket_object(bucket_name: 'inspec-jumpstart', key: 'web01.html') do
-it { should exist }
-It { should be_public }
- end
+  describe aws_s3_bucket_object(bucket_name: 'inspec-jumpstart', key: 'web01.html') do
+    it { should exist }
+    it { should be_public }
+  end
 end 
