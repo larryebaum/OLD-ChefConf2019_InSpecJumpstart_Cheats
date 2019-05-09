f003c3753e764cd54417a0ee52d196d29bad4e5b
Larry Eichenbaum <larryebaum@getchef.com>
2019-05-09 09:15:54 -0400

    bucket name

--- control_aws.rb
+++ control_aws.rb
@@ -6,7 +6,7 @@ control 'myaws-01' do
     it { should be_running}
   end
 
-  describe aws_s3_bucket_object(bucket_name: 'inspec-jumpstart', key: 'web01.html') do
+  describe aws_s3_bucket_object(bucket_name: 'inspec-jumpstart-2019', key: 'web01.html') do
     it { should exist }
     it { should be_public }
   end
